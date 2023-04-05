2709928055d42f0bad30c29e8c070d021b4f32e1
rogerrm95 <rogerrm@live.com>
2023-04-04 23:18:27 -0300

    Cadastro de usuário - Estrutura visual da home

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -9,21 +9,20 @@ export default function Document() {
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
-          rel="stylesheet"
+          crossOrigin=""
         />
 
         <style
           id="stitches"
           dangerouslySetInnerHTML={{ __html: getCssText() }}
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
-        <Main></Main>
+        <Main />
         <NextScript />
       </body>
     </Html>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,21 +1,28 @@
 import Image from 'next/image'
+import AppPreviewImage from '../../assets/app-preview.png'
 
+import { Heading, Text } from '@ignite-ui/react'
 import { Container, Hero, Preview } from './styles'
-import { Heading } from '@ignite-ui/react'
 
 export default function Home() {
   return (
     <Container>
       <Hero>
-        <Heading>Olá</Heading>
+        <Heading as="h1" size="4xl">
+          Agendamento descomplicado
+        </Heading>
+        <Text size="lg">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
       </Hero>
 
       <Preview>
         <Image
-          src=""
-          priority
-          height={500}
+          src={AppPreviewImage}
+          height={400}
           quality={100}
+          priority
           alt="Imagem da aplicação de Agenda online"
         />
       </Preview>
