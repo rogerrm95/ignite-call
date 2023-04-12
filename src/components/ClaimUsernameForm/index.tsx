230ba341dd9e2a57da5e489e6fa933efb0322677
rogerrm95 <rogerrm@live.com>
2023-04-11 21:18:20 -0300

    Cadastro de usuário - Utilizando React Hook Form

--- src/components/ClaimUsernameForm/index.tsx
+++ src/components/ClaimUsernameForm/index.tsx
@@ -1,12 +1,32 @@
+import { useForm } from 'react-hook-form'
+import z from 'zod'
+
 import { Button, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
 
 import { Form } from './styles'
 
+const claimUsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
+
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuario" />
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuario"
+        {...register('username')}
+      />
       <Button type="submit" size="sm">
         Reservar
         <ArrowRight weight="bold" />
