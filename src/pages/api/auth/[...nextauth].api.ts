fb687d6cf5ceca633e3a7aabeed432e642de8edc
rogerrm95 <rogerrm@live.com>
2023-04-16 19:05:28 -0300

    Conexão com calendário - Conteúdo da sessão do Next Auth

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -41,6 +41,13 @@ export function buildNextAuthOption(
 
         return true
       },
+
+      async session({ session, user }) {
+        return {
+          ...session,
+          user,
+        }
+      },
     },
   }
 }
