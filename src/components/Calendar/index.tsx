cf3fb4d9e08a337ff09a36fffe0a774c60af4fb1
rogerrm95 <rogerrm@live.com>
2023-05-01 22:06:36 -0300

    Configurando disponibilidade - Persistindo intervalos #2

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -119,7 +119,7 @@ export function Calendar({ onDateSelected, selectedDate }: CalendarProps) {
           date,
           disabled:
             date.endOf('day').isBefore(new Date()) ||
-            blockedDates?.blockedWeekDays.includes(date.get('day')),
+            blockedDates.blockedWeekDays.includes(date.get('day')),
         }
       }),
       ...nextMonthFillArray.map((date) => {
