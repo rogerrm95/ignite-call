90dd4560db615f0eca4cef31bb72e120a8300027
Rogério Marques <rogerrm@live.com>
2023-05-10 21:25:18 -0300

    Agendamento - Criação do evento no Google01~

--- src/pages/api/users/[username]/schedule.api.ts
+++ src/pages/api/users/[username]/schedule.api.ts
@@ -2,6 +2,8 @@ import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
 import { NextApiRequest, NextApiResponse } from 'next'
 import dayjs from 'dayjs'
+import { google } from 'googleapis'
+import { getGoogleOAuthToken } from '@/lib/google'
 
 export default async function handler(
   req: NextApiRequest,
@@ -55,13 +57,42 @@ export default async function handler(
     })
   }
 
-  await prisma.scheduling.create({
+  const scheduling = await prisma.scheduling.create({
     data: {
       date: schedulingDate.toDate(),
       name,
       email,
       observations: observations || null,
       user_id: user.id,
+    },
+  })
+
+  const calendar = google.calendar({
+    version: 'v3',
+    auth: await getGoogleOAuthToken(user.id),
+  })
+
+  await calendar.events.insert({
+    calendarId: 'primary',
+    conferenceDataVersion: 1,
+    requestBody: {
+      summary: `Ignite Call: ${name}`,
+      description: observations,
+      start: {
+        dateTime: schedulingDate.format(),
+      },
+      end: {
+        dateTime: schedulingDate.add(1, 'hour').format(),
+      },
+      attendees: [{ email, displayName: name }],
+      conferenceData: {
+        createRequest: {
+          requestId: scheduling.id,
+          conferenceSolutionKey: {
+            type: 'hangoutsMeet',
+          },
+        },
+      },
     },
   })
 
